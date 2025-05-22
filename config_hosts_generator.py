9d436e90f562600c2ad1973fbd8f0538e9397ef8
Mounika chaya <chaya.mudragada99@gmail.com>
2025-05-22 22:00:31 +0000

    Added operate and modified the config file
    Modified config_generator file such that it takes the server
    name and returns dict with names as key and values as ip address.
    Modifying existing hosts and config file is little tricky and
    needs more code.
    
    Also added the operate file if a server is deleted or number of
    servers required is equal to the servers active case is handled.

--- config_hosts_generator.py
+++ config_hosts_generator.py
@@ -1,7 +1,12 @@
+import re
+
 def write_ansible_and_ssh_config(hosts_dict, tag, identity_file_path):
     bastion_key = f"{tag}_bastion"
     haproxy_key = f"{tag}_haproxy"
-    dev_keys = [f"{tag}_dev{i}" for i in range(1, 4)]
+    # Match keys like "mytag_dev1", "mytag_dev2", etc.
+    dev_pattern = re.compile(rf"^{tag}_dev\d+$")
+    dev_keys = [key for key in hosts_dict if dev_pattern.match(key)]
+
 
     # === Ansible Hosts File ===
     hosts_file_content = f"""[Bastion]
@@ -53,3 +58,27 @@ Host {haproxy_key}
         f.write(config_file_content)
 
     print(f"Files written: 'hosts' and '{tag}_config'")
+
+
+def extract_names_ips_from_server(total_servers, tag):
+    server_dict = {}
+
+    for server in total_servers:
+        addresses = server.addresses.get(f"{tag}network", [])
+        
+        if server.name == f"{tag}_bastion":
+            # Extract floating IPv4 address
+            ip = next(
+                (addr["addr"] for addr in addresses if addr["version"] == 4 and addr["OS-EXT-IPS:type"] == "floating"),
+                None
+            )
+        else:
+            # Extract fixed IPv4 address
+            ip = next(
+                (addr["addr"] for addr in addresses if addr["version"] == 4 and addr["OS-EXT-IPS:type"] == "fixed"),
+                None
+            )
+        
+        server_dict[server.name] = ip
+
+    return server_dict
